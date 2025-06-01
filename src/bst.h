168a9e099d542fa4b0a200887f675a60f101c6ee
CarlosDanielSLima <carlitosdellima@gmail.com>
2025-05-31 22:35:01 -0300

    Update: Transfer Node and BinaryTree structs and functions to their respective trees.

--- src/bst.h
+++ src/bst.h
@@ -2,14 +2,37 @@
 #ifndef BST_H
 #define BST_H
 
+#include <iostream>
+#include <vector>
 #include "tree_utils.h"
 
+using std::cin, std::cout, std::endl, std::string, std::vector;
+using namespace TREE_UTILS;
+
 namespace BST {
-    BinaryTree* create();
-    InsertResult insert(BinaryTree* tree, const std::string& word, int documentId);
-    SearchResult search(BinaryTree* tree, const std::string& word);
+    struct Node {
+        string word;
+        vector<int> documentIds;
+        Node* parent;
+        Node* left;
+        Node* right;
+    };
+
+
+    struct BinarySearchTree {
+        Node* root;
+    };
+
+    /**
+     * @brief Initialize a binary search tree.
+     * @return BST created.
+     */
+    BinarySearchTree* create();
+
+    InsertResult insert(BinaryTree* tree, const string& word, int documentId);
+
+    SearchResult search(BinaryTree* tree, const string& word);
     void destroy(BinaryTree* tree); // liberar memória
 }
 
 #endif
-
--- src/tree_utils.cpp
+++ src/tree_utils.cpp
@@ -1,33 +1,7 @@
 #include <string>
 #include <vector>
+#include "tree_utils.h"
 
-
-struct Node {
-    std::string word;
-    std::vector<int> documentIds;
-    Node* parent;
-    Node* left;
-    Node* right;
-    int height;    // usado na AVL
-    int isRed;     // usado na RBT
-};
-
-struct BinaryTree {
-    Node* root;
-    Node* NIL;  // usado na RBT (Opcional)
-};
-
-
-struct InsertResult {
-    int numComparisons;
-    double executionTime;
-    // <Possíveis outras variáveis>
-};
-
-struct SearchResult {
-    int found;
-    std::vector<int> documentIds;
-    double executionTime;
-    int numComparisons;
-    // <Possíveis outras variáveis>
-};
+namespace TREE_UTILS {
+    
+}
