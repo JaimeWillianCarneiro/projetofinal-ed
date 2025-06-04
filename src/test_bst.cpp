06adaae93806e3e582e030bd28bff52b99857d93
Gabrielly Chácara <gabriellyepc@gmail.com>
2025-06-04 16:38:16 -0300

    atualizei o test_bst

--- src/test_bst.cpp
+++ src/test_bst.cpp
@@ -7,7 +7,8 @@
 using namespace std;
 using namespace BST;
 
-void printSearchResult(const SearchResult& result, const string& word) { // função para deixar o código mais simples
+// função para deixar o código mais simples, já que serão testadas muitas palavras
+void printSearchResult(const SearchResult& result, const string& word) {
     cout << "busca por \"" << word << "\": ";
     if (result.found) { // caso a palavra seja encontrada 
         cout << "documentos: ";
@@ -23,28 +24,102 @@ void printSearchResult(const SearchResult& result, const string& word) { // fun�
 int main() {
     BinaryTree* tree = create(); // cria a arvore inicial vazia
 
-    // insercao das palavras. peguei uma lista aleatoria de nomes de animais
-    insert(tree, "cachorro", 1);
-    insert(tree, "gato", 2);
-    insert(tree, "elefante", 3);
-    insert(tree, "gato", 4);      // repetida com outro documento
-    insert(tree, "abelha", 2);
-    insert(tree, "zebra", 5);
-    insert(tree, "cobra", 3);
-    insert(tree, "cachorro", 6);  // repetida com outro documento
+    // insercao das palavras. peguei uma lista de frases aleatorias e citacoes famosas
+    insert(tree, "penso", 1);
+    insert(tree, "logo", 1);
+    insert(tree, "existo", 1);
+
+    insert(tree, "seja", 2);
+    insert(tree, "a", 2);
+    insert(tree, "mudanca", 2);
+    insert(tree, "que", 2);
+    insert(tree, "voce", 2);
+    insert(tree, "deseja", 2);
+    insert(tree, "ver", 2);
+    insert(tree, "no", 2);
+    insert(tree, "mundo", 2);
+
+    insert(tree, "so", 3);
+    insert(tree, "sei", 3);
+    insert(tree, "que", 3);
+    insert(tree, "nada", 3);
+    insert(tree, "sei", 3);
+
+    insert(tree, "a", 4);
+    insert(tree, "felicidade", 4);
+    insert(tree, "esta", 4);
+    insert(tree, "nas", 4);
+    insert(tree, "coisas", 4);
+    insert(tree, "simples", 4);
+
+    insert(tree, "o", 5);
+    insert(tree, "olhar", 5);
+    insert(tree, "diz", 5);
+    insert(tree, "muito", 5);
+    insert(tree, "um", 5);
+    insert(tree, "olhar", 5);
+    insert(tree, "pode", 5);
+    insert(tree, "mudar", 5);
+    insert(tree, "tudo", 5);
+
+    insert(tree, "o", 6);
+    insert(tree, "tempo", 6);
+    insert(tree, "nao", 6);
+    insert(tree, "para", 6);
+    insert(tree, "de", 6);
+    insert(tree, "tempo", 6);
+    insert(tree, "ao", 6);
+    insert(tree, "tempo", 6);
+
+    insert(tree, "encontre", 7);
+    insert(tree, "seu", 7);
+    insert(tree, "caminho", 7);
+    insert(tree, "ha", 7);
+    insert(tree, "um", 7);
+    insert(tree, "caminho", 7);
+    insert(tree, "novo", 7);
+
+    insert(tree, "ser", 8);
+    insert(tree, "livre", 8);
+    insert(tree, "e", 8);
+    insert(tree, "preciso", 8);
+    insert(tree, "deixe", 8);
+    insert(tree, "ser", 8);
+    insert(tree, "o", 8);
+    insert(tree, "que", 8);
+    insert(tree, "for", 8);
+
+    insert(tree, "busque", 9);
+    insert(tree, "a", 9);
+    insert(tree, "sua", 9);
+    insert(tree, "luz", 9);
+    insert(tree, "a", 9);
+    insert(tree, "luz", 9);
+    insert(tree, "guia", 9);
+    insert(tree, "o", 9);
+    insert(tree, "passo", 9);
 
     cout << "\n Impressao da arvore:" << endl; 
     printTree(tree);  // mostra a arvore completa
 
     cout << "\n Buscas:" << endl;
-    printSearchResult(search(tree, "gato"), "gato");           // saida: 2 4
-    printSearchResult(search(tree, "cachorro"), "cachorro");   // saida: 1 6
-    printSearchResult(search(tree, "elefante"), "elefante");   // saida: 3
-    printSearchResult(search(tree, "cobra"), "cobra");         // saida: 3
-    printSearchResult(search(tree, "tigre"), "tigre");         // saida: "esta palavra nao esta inclusa"
-    printSearchResult(search(tree, "zebra"), "zebra");         // saida: 5
-    printSearchResult(search(tree, "abelha"), "abelha");       // saida: 2
+    printSearchResult(search(tree, "tempo"), "tempo");         // saida: 6
+    printSearchResult(search(tree, "que"), "que");             // saida: 2 3 8
+    printSearchResult(search(tree, "elefante"), "elefante");   // saida: "esta palavra nao esta inclusa"
+    printSearchResult(search(tree, "para"), "para");           // saida: 6
+    printSearchResult(search(tree, "alegria"), "alegria");     // saida: "esta palavra nao esta inclusa"
+    printSearchResult(search(tree, "mudanca"), "mudanca");     // saida: 2
+    printSearchResult(search(tree, "sei"), "sei");             // saida: 3
+    printSearchResult(search(tree, "arvore"), "arvore");       // saida: "esta palavra nao esta inclusa"
+    printSearchResult(search(tree, "existo"), "existo");       // saida: 1
+    printSearchResult(search(tree, "for"), "for");             // saida: 8
+    printSearchResult(search(tree, "caminho"), "caminho");     // saida: 7
+    printSearchResult(search(tree, "professor"), "professor"); // saida: "esta palavra nao esta inclusa"
+    printSearchResult(search(tree, "o"), "o");                 // saida: 5 6 8 9
+    printSearchResult(search(tree, "amor"), "amor");           // saida: "esta palavra nao esta inclusa"
+    printSearchResult(search(tree, "encontre"), "encontre");   // saida: 7
+    printSearchResult(search(tree, "a"), "a");                 // saida: 2 4 9
 
-    destroy(tree); // destroi a arvore assim que o código termina de rodar
+    destroy(tree); // destroi a arvore liberando espaço novamente
     return 0;
 }
