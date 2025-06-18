88482eafe8d911c7dac831d886686caf55fbe191
gabi-bs <gabrielabarbosa90852@gmail.com>
2025-06-18 16:06:13 -0300

    Teste busca palavras BST

--- src/test_bst.cpp
+++ src/test_bst.cpp
@@ -26,8 +26,8 @@ void printTreeWithTitle(BinaryTree* tree, const string& title) {
     cout << "------------------------\n";
 }
 
-void testInsertions(BinaryTree* tree) {
-    cout << "\n Inserindo palavras...\n";
+void testInsertions1(BinaryTree* tree) {
+    cout << "\n[1] Inserindo palavras...\n";
     insert(tree, "penso", 1);
     insert(tree, "logo", 1);
     insert(tree, "existo", 1);
@@ -39,8 +39,44 @@ void testInsertions(BinaryTree* tree) {
     printTreeWithTitle(tree, "Arvore apos insercao inicial");
 }
 
+void testInsertions2(BinaryTree* tree) {
+    cout << "\n[2] Inserindo palavras...\n";
+    insert(tree, "a", 4);
+    insert(tree, "felicidade", 4);
+    insert(tree, "esta", 4);
+    
+    insert(tree, "o", 5);
+    insert(tree, "olhar", 5);
+    insert(tree, "diz", 5);
+    
+    insert(tree, "o", 6);
+    insert(tree, "tempo", 6);
+    insert(tree, "nao", 6);
+    
+    printTreeWithTitle(tree, "Arvore apos novas insercoes");
+}
+
+void testSearches(BinaryTree* tree) {
+    cout << "\n***** TESTANDO BUSCAS *****\n";
+    
+    printSearchResult(search(tree, "tempo"), "tempo");
+    printSearchResult(search(tree, "que"), "que");
+    printSearchResult(search(tree, "elefante"), "elefante");
+    
+    // Testes com palavras repetidas
+    printSearchResult(search(tree, "a"), "a");
+    printSearchResult(search(tree, "o"), "o");
+    
+    // Testes com palavras não existentes
+    printSearchResult(search(tree, "walleria"), "walleria");
+    printSearchResult(search(tree, "jaime"), "jaime");
+}
+
 int main() {
     BinaryTree* tree = create();
-    testInsertions(tree);
+    testInsertions1(tree);
+    testInsertions2(tree);
+    testSearches(tree);
+
     return 0;
 }
